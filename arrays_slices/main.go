f83c50be93e14da5eb20083dac448a09edc9c030
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-06-18 17:34:50 +0300

    arrays

---
+++ arrays_slices/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	/* var names [3]string
+
+	names[0] = "Mehmetcan"
+	names[1] = "Ece"
+	names[2] = "Ali" */
+
+	var names =[3] string{"Mehmetcan", "Ece", "Ali"}	
+
+	names[0] = "Serhat"
+
+	names[3] = "Zeynep" // This will cause a compile-time error because the array size is fixed at 3.
+
+
+	fmt.Println(names)
+}
