c1cfdd6a6334cafb59cd1716886dc1437f416141
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-06-22 19:58:07 +0300

    arrays-slices

--- arrays_slices/main.go
+++ arrays_slices/main.go
@@ -10,9 +10,10 @@ func main(){
 	names[1] = "Ece"
 	names[2] = "Ali" */
 
-	var names =[3] string{"Mehmetcan", "Ece", "Ali"}	
+	var names =[3] string{"Mehmetcan", "Ece", "Ali","Jack"}	
 
 	names[0] = "Serhat"
+	names[1] = " Mehmetcan"
 
 	names[3] = "Zeynep" // This will cause a compile-time error because the array size is fixed at 3.
 
