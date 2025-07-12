c72f60ba0aab47ad58eae35f55796765017e8879
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-07-12 19:23:07 +0300

    if else

--- conditionals/main.go
+++ conditionals/main.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 
 func main() {
+
+	var age = 17
+
+	if age >= 18 {
+		fmt.Println("you can vote.")
+	} else { //else burada yazılmalı aşağı satırda yazarsan syntax error alıyorsun
+		fmt.Println("you cannot vote.")
+	}
 
 }
 
