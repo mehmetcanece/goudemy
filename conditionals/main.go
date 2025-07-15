f17145820d636c440dbe91aae460bfa6d830f024
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-07-16 01:07:35 +0300

    conditionals

--- conditionals/main.go
+++ conditionals/main.go
@@ -5,13 +5,31 @@ import "fmt"
 
 func main() {
 
-	var age = 17
+	/* var age = 17
 
 	if age >= 18 {
 		fmt.Println("you can vote.")
 	} else { //else burada yazılmalı aşağı satırda yazarsan syntax error alıyorsun
 		fmt.Println("you cannot vote.")
+	} */
+
+	a := 9
+	b := 9
+	c := 3
+
+
+	if a>=b && a>=c {
+		fmt.Println("a is the biggest one")
+	} else if b >= a && b >= c {
+		fmt.Println("b is the biggest one")
+	} else if c>=a && c>=b {
+		fmt.Println("c is the biggest one")
 	}
+
+
+
+
+	
 
 }
 
