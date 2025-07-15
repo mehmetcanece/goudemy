71e72767542e4eb5e9bef10c7668900324ab04f1
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-07-16 01:23:52 +0300

    break/continue

--- for_loops/main.go
+++ for_loops/main.go
@@ -19,14 +19,33 @@ func main(){
 	fmt.Println("Total:", total)
  } */
 
-index := 0
+/* index := 0
 
 var names = [3]string{"Mehmet","Can","Ece"}
 
 for index<len(names) {
 	fmt.Println(names[index])
 	index++
+} */
+
+
+
+/* for index := 0; index <=10; index++ {
+	if index == 3 {
+		break
+	}
+	fmt.Println(index)
+} */
+
+
+for index := 0; index <=10 ; index++{
+	if index ==2 || index ==5 {
+		continue
+	}
+	fmt.Println(index)
 }
+
+
 
 
 
