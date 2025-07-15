81b037e68aabf809dad46fb8bb5ddd2b35918b7c
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-07-16 02:28:22 +0300

    iki ciktili func

--- functions/main.go
+++ functions/main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main(){
-	total := add(10,20)
-	fmt.Println(total)
+	/* total := add(10,20)
+	fmt.Println(total) */
+
+	total, difference := calculation(10,20)
+	fmt.Println(total,difference)
 }
 
 func add(x int, y int) int{
 	//fmt.Println(x+y)
 	return x + y
 }
+
+
+func calculation(x int, y int) (int, int){
+	return x + y, x - y
+} //iki farklı değer de dönebilir 
