9975ca554c393b49d0af17218029905e10b33746
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-07-18 15:23:22 +0300

    pointer

--- pointers/main.go
+++ pointers/main.go
@@ -9,13 +9,19 @@ func main(){
 
 	var p *int
 
+	var j *int
+
 	a = 10
 
 	p = &a // a'nın adresini p'ye atadık
 
+	j = p // a'nın adresini j'ye de atadık
+
 	fmt.Println(a)
 
 	fmt.Println(p)
+
+	fmt.Println(j)
 
 	fmt.Println(*p) // p'nin işaret ettiği değeri yazdırdık. 
 	//& işareti adresi veriyor, 
