3049be875c4c9fa1527c12b3b399cfbc6ae17233
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-07-23 18:59:50 +0300

    pointers

--- pointers/main.go
+++ pointers/main.go
@@ -5,17 +5,34 @@ import "fmt"
 func main(){
 
 
-	var a int
+	var a  = 10
+	var b int
+	var p *int
+	
+	b=a
+	p= &a // a'nın adresini p'ye atadık
+
+	*p=20
+
+	fmt.Println(a,b)
+
+
+
+	/* var a int
 
 	var p *int
 
-	var j *int
+	//var j *int
 
 	a = 10
 
 	p = &a // a'nın adresini p'ye atadık
 
-	j = p // a'nın adresini j'ye de atadık
+	*p = 20 // p'nin işaret ettiği değeri 20 yaptık
+
+	fmt.Println(a) */
+
+	/* j = p // a'nın adresini j'ye de atadık
 
 	fmt.Println(a)
 
@@ -23,8 +40,8 @@ func main(){
 
 	fmt.Println(j)
 
-	fmt.Println(*p) // p'nin işaret ettiği değeri yazdırdık. 
-	//& işareti adresi veriyor, 
+	fmt.Println(*p)  */// p'nin işaret ettiği değeri yazdırdık. 
+	//& işareti adresi veriyor, dereferencing
 	
 	// * işareti ise o adresin içindeki değeri veriyor.
 
